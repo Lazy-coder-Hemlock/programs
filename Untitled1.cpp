c97acb56b3a3bccadd528b39d77f09d27bf1b3d2
Mayank singh Pathania <39670439+Hemlock420@users.noreply.github.com>
2020-03-06 11:50:05 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#incluce<opencv/opencv2.hpp>
+using namespace std;
+int main(){
+	//a section
+//	string username;
+//	string password;
+//	int tries=3;
+//	int success=0;
+//	while(tries and !success){
+//		if(username=="random" and password=="123456")
+//		{
+//			system("cls");
+//			cout<<"You got access"<<endl;
+//			success=1;
+//		}
+//		else
+//		{
+//			tries--;
+//		}
+//	}
+// B and E section
+//	int record=10;
+//	Videocapture cap(0);
+//	if(!cap.isOpened())
+//	return -1;
+//	string f=VideoWriter_fourcc(*"XVID");
+//	string out=VideoWriter("store.mp4",f,20,0,(640,80));
+//	while(record--){
+//		Mat frame;
+//		cap.read(frame);
+//		imshow("camera",frame);
+//		if(waitKey(20)=='q'){
+//			break;
+//		}
+//	}
+//C and D section
+//	Mat src=imread("example.png");
+//	Mat planes[3];//divides into RGB
+//	split(src,planes);//planes[2] is red
+//	Mat image=imread("example.png");
+//	if(image.empty()){
+//		cout<<"Not find the image"<<endl;
+//	}
+//	Mat new1;
+//	GaussianBlur(image,new1,size(3,3),0);
+//	imshow(new1);
+//	
+//	waitKey(0);
+//	destroyAllWindows();
+//	
+//	return 0;
+}
